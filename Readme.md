94fefe80ef949ba05c17415198d2c3dfc5c981fd
ultimodruido <public@nicolinux.it>
2025-02-15 22:46:23 +0100

    First commit - starting Readme.md description - primordial working dongle reader - basis timer

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Oxigen python library
+A simple generic library to interact with the oxigen dongle (version 4)
+
+> [!WARNING]
+> This project is in an embryonic stage. Function and class signatures are not stable
+
+## Goals
+Develop 
+
+Features in current plan:
+- do not keep track of race information, this task is demanded to the RMS
+- decode dongle protocol and expose a simple class with the received state of all data in a Car-Controller pair
+- offer a signal/slot messaging system (in Qt-style) to inform the RMS about changing in the car state
+- implement a simple timer for time alignment with the dongle (with optional timer value injection from RMS)
---
+++ racetimer.py
@@ -0,0 +1,52 @@
+"""
+Simple Timer for providing a time reference in the messages to the dongle
+"""
+from time import monotonic_ns
+from struct import pack
+
+class RaceTimer:
+    def __init__(self):
+        self._starting_time = 0
+        self._paused_time = 0
+        self._stopped_time = 0
+        self._running = False
+
+    def start(self):
+        if not self._running:
+            self._stopped_time = 0
+            self._starting_time = monotonic_ns()
+            self._running = True
+
+    def pause(self):
+        if self._running:
+            self._paused_time = (monotonic_ns() - self._starting_time) + self._paused_time
+            self._stopped_time = self._paused_time
+            self._running = False
+
+    def resume(self):
+        if self._paused_time != 0:
+            self.start()
+
+    def stop(self):
+        if self._running:
+            self._stopped_time = (monotonic_ns() - self._starting_time) + self._paused_time
+            self._paused_time = 0
+            self._running = False
+
+    def value(self):
+        if self._running:
+            return (monotonic_ns() - self._starting_time) + self._paused_time
+        else:
+            return self._stopped_time
+
+    def value_cs(self):
+        return self.value() // 10**7
+
+    def value_cs_bytes(self):
+        time = self.value_cs()
+        # convert to 4 bytes with pack and straight to int with list
+        bytes_list = list(pack('>i', time))
+        # return only 3 bytes as oxigen protocol count time as 24bits int
+        # skip the hoghest one
+        return bytes_list[1:]
+
