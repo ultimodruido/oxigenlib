346a77738586a0ffc0827637b79919bfa4a692c8
ultimodruido <public@nicolinux.it>
2025-03-16 20:26:06 +0100

    updated import to properly initiate the library

--- src/oxigenlib/__init__.py
+++ src/oxigenlib/__init__.py
@@ -7,8 +7,11 @@ TODO: add description
 #from .dongle import Dongle
 
 from .config import RaceState, Command, PitLaneTrigger, PitLaneCount, PowerMeanValue
+from .config import O2Config, O2RaceStatus, OxigenSystem
 from .events import oxigen_events as events
 from .racers import oxigen_racers as racers
 from .dongle import oxigen_dongle as dongle
+from .racetimer import RaceTimer
 from .utils import set_race_state
+
 
